1a4fb06f1b01c832d20d4792221ea7ce3436bfee
С - 14 < - 14@DESKTOP-O8LQCCI>
2023-09-26 21:05:30 +0500

    Добавьте файлы проекта.

---
+++ BD_MSSQL/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BD_MSSQL
+{
+    public partial class Form1 : Form
+    {
+        private string ConnectionString;
+        private SqlConnection connection = null;
+        private string SQLExpression = "SELECT * FROM metrics";
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbConnectionStr.Text = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;";
+        }
+    }
+}
